0526a381d7f4737bfb72ba80d428c1146bf859a2
Jason Carter <jasoncarter30@outlook.com>
2020-05-05 12:06:02 -0500

    Checking in latest changes for Dashboard.js.

--- JasonCarter.BudgetDashboard.Web/wwwroot/js/Dashboard.js
+++ JasonCarter.BudgetDashboard.Web/wwwroot/js/Dashboard.js
@@ -4,6 +4,40 @@
 
     var pageLoad = function () {
         _utilitiesOverviewData = model.UtililitesOverview;
+
+        $("#UtilitiesOverview").kendoChart({
+            legend: {
+                position: "bottom"
+            },
+            seriesDefaults: {
+                type: "line",
+                style: "smooth",
+            },
+            dataSource: {
+                data: _utilitiesOverviewData,
+                group: { field: 'Year' },
+            },
+            chartArea: {
+                width: '600px',
+                height: '400px'
+            },
+            tooltip: {
+                visible: true,
+                format: "{0:c2}",
+                template: "#= series.name #: #= kendo.toString(value, 'c') #"
+            },
+            theme: 'bootstrap',
+            series: [{ type: 'line', field: 'Amount', categoryField: 'Month' }],
+            //seriesClick: getGasSpendingBreakdown,
+            categoryAxis: [
+                {
+                    labels: {
+                        rotation: "auto"
+                    }
+                }
+            ]
+        });
+
     }
 
 
